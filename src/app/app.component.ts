830b4747bfbcf2a0c2fa88ea853e00d3b47204e0
Mohamad Al Maleh <mohammad.maleh@gmail.com>
2023-11-11 04:57:04 +0100

    feature/adding-html-templates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
-
+console.log('here');
 @Component({
   selector: 'app-root',
+
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
